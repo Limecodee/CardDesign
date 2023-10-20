0af9a8664e691d61aeba5b480ddd697984fee309
Limecodee <89806576+Limecodee@users.noreply.github.com>
2023-10-20 14:21:49 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,1 @@
-document.addEventListener('DOMContentLoaded', (event) => {
-  let flipCards = document.querySelectorAll('.flip-card');
-
-  flipCards.forEach((card) => {
-    card.addEventListener('click', function () {
-      console.log("Card clicked!");  // This line will log a message to the console when the card is clicked
-      let innerCard = this.querySelector('.flip-card-inner');
-      innerCard.style.transform = innerCard.style.transform === 'rotateY(180deg)' ? 'rotateY(0deg)' : 'rotateY(180deg)';
-    });
-  });
-});
 
