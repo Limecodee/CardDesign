157719a68330e41b71d4a918307b70b5367ce385
Limecodee <89806576+Limecodee@users.noreply.github.com>
2023-10-20 14:03:39 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,14 +1,28 @@
-function checkAnswer() {
-    const answerInput = document.getElementById('answerInput');
-    const flashcard = document.getElementById('flashcard');
-  
-    if (answerInput.value.toLowerCase() === 'paris') {
-      flashcard.classList.add('correct');
-      flashcard.classList.remove('incorrect');
-    } else {
-      flashcard.classList.add('incorrect');
-      flashcard.classList.remove('correct');
-    }
-  }
-  
-  
+document.addEventListener('DOMContentLoaded', function() {
+
+    let draggedElement;
+
+    // Add event listeners to draggable elements
+    const draggables = document.querySelectorAll('.draggable');
+    draggables.forEach((draggable) => {
+        draggable.addEventListener('dragstart', function(event) {
+            draggedElement = event.target;
+        });
+    });
+
+    // Add event listeners to droppable elements
+    const droppables = document.querySelectorAll('.droppable');
+    droppables.forEach((droppable) => {
+        droppable.addEventListener('dragover', function(event) {
+            event.preventDefault();
+        });
+        
+        droppable.addEventListener('drop', function(event) {
+            if (draggedElement.textContent.trim() === event.target.textContent.trim()) {
+                event.target.style.display = 'none';
+                draggedElement.style.display = 'none';
+            }
+        });
+    });
+});
+
