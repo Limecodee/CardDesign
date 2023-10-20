10403077cd5a05fd11a357fd18bb93db309a14f9
Limecodee <emilbarca@outlook.com>
2023-10-20 17:48:03 +0200

    Update FlashCard1 with question will always will be front page

--- FlashCard1/index1.js
+++ FlashCard1/index1.js
@@ -1,12 +1,40 @@
-document.addEventListener('DOMContentLoaded', (event) => {
-    let flipCards = document.querySelectorAll('.flip-card');
-  
-    flipCards.forEach((card) => {
-      card.addEventListener('click', function () {
-        console.log("Card clicked!");  // This line will log a message to the console when the card is clicked
-        let innerCard = this.querySelector('.flip-card-inner');
-        innerCard.style.transform = innerCard.style.transform === 'rotateY(180deg)' ? 'rotateY(0deg)' : 'rotateY(180deg)';
-      });
+document.addEventListener('DOMContentLoaded', function() {
+  // Get the carousel element
+  var carousel = document.getElementById('carouselExampleControls');
+
+  // Function to attach click events to flip-cards
+  function attachFlipEvents() {
+    var flipCards = document.querySelectorAll('.flip-card');
+    flipCards.forEach(function(card) {
+      card.removeEventListener('click', flipCard);  // Remove existing event to prevent duplication
+      card.addEventListener('click', flipCard);  // Attach new event
     });
+  }
+
+  // Initial attachment of click events
+  attachFlipEvents();
+
+  // Listen for the 'slid.bs.carousel' event
+  carousel.addEventListener('slid.bs.carousel', function() {
+    // Reset all flip-cards to their original state
+    var flipCards = document.querySelectorAll('.flip-card-inner');
+    flipCards.forEach(function(card) {
+      card.style.transform = 'rotateY(0deg)';
+    });
+
+    // Re-attach click events after sliding
+    attachFlipEvents();
   });
+});
+
+// Function to flip the card
+function flipCard(event) {
+  var innerCard = event.currentTarget.querySelector('.flip-card-inner');
+  var isFlipped = innerCard.style.transform === 'rotateY(180deg)';
+  innerCard.style.transform = isFlipped ? 'rotateY(0deg)' : 'rotateY(180deg)';
+}
+
+
+
+
   
